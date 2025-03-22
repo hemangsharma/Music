a08bc9564bf1e242fb01235660dc38d189255b0e
hemangsharma <sharmahemang2000@gmail.com>
2025-03-22 15:13:44 +1100

    1st updated

---
+++ Music/MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicApp.swift
+//  Music
+//
+//  Created by Hemang Sharma on 22/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicTests/MusicTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicTests.swift
+//  MusicTests
+//
+//  Created by Hemang Sharma on 22/03/2025.
+//
+
+import Testing
+@testable import Music
+
+struct MusicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
